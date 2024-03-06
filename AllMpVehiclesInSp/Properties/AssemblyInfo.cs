a995b6b7aff201b9c9f18f0b6e5ab376768ff242
Phyksar <phyksar@gmail.com>
2024-03-06 15:37:55 +0300

    Strip spaces from filenames

--- AllMpVehiclesInSp/Properties/AssemblyInfo.cs
+++ AllMpVehiclesInSp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("All MP Vehicles in SP")]
+[assembly: AssemblyTitle("AllMpVehiclesInSp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("All MP Vehicles in SP")]
+[assembly: AssemblyProduct("AllMpVehiclesInSp")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
